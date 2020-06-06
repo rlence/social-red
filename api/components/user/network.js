546138a0201b5c1694d4cf07b12a921a536f0391
rlence <lencericardo@gmail.com>
2020-06-06 17:32:32 +0200

    creanddo store y primeras rutas, volviendo funciones de store async

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,12 +1,26 @@
 const express = require('express');
 
 const response = require('../../../network/response');
+const Controller = require('./index');
+
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    response.succes(req, res, 'Todo correcto', 200)
-})
+
+    Controller.list()
+    .then( list => response.succes(req, res, list, 200))
+    .catch( err => response.error(req, res, err.message, 500));
+
+});
+
+router.get('/:id', (req, res) => {
+
+    Controller.get(req.params.id)
+    .then( user => response.succes(req, res, user, 200))
+    .catch( err => response.error(req, res, err.message, 500));
+
+});
 
 
 module.exports = router;
